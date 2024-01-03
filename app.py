1ab01e2948026e03df9067edaa9ec035f511e7b0
Abhi01goenka <abhishekgoenka4@gmail.com>
2024-01-03 18:11:26 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+with app.app_context():
+    db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    # nullable = false means cannot be null
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method=="POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
+
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    alltodo = Todo.query.all()
+    return render_template("index.html", alltodo=alltodo)
+    # alltodo=alltodo now making it available for index.html
+
+@app.route('/show')
+def fun1():
+    alltodo = Todo.query.all()
+    print(alltodo)
+    return "show page"
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        title = request.form["title"]
+        desc = request.form["desc"]
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html", todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
